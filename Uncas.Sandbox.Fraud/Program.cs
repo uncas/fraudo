c02644869f1ea30a6522f5a4996a9dc6a5542ab4
Ole Lynge Soerensen <olelynge@gmail.com>
2012-05-04 23:23:13 +0200

    Refactor theta to list.

--- Uncas.Sandbox.Fraud/Program.cs
+++ Uncas.Sandbox.Fraud/Program.cs
@@ -6,13 +6,12 @@ namespace Uncas.Sandbox.Fraud
 {
     public class Program
     {
-        private static void Main(string[] args)
+        private static void Main()
         {
             const double alpha = 0.5d;
 
             // Guess at theta
-            double theta0 = 0.1d;
-            double theta1 = 1d;
+            IEnumerable<double> theta = new[] {0.1d, 1d}.ToList();
 
             IList<Comment> comments = new CommentRepository().GetComments();
             var badWordFeature = new BadWordFeature();
@@ -23,7 +22,7 @@ namespace Uncas.Sandbox.Fraud
             for (int i = 0; i < 100; i++)
             {
                 foreach (var sample in samples)
-                    sample.Probability = Probability(sample, theta0, theta1);
+                    sample.Probability = Probability(sample, theta);
                 double sum0 = 0d;
                 double sum1 = 0d;
                 foreach (var sample in samples)
@@ -34,20 +33,19 @@ namespace Uncas.Sandbox.Fraud
 
                 double delta0 = -alpha*sum0;
                 double delta1 = -alpha*sum1;
-
-                theta0 += delta0;
-                theta1 += delta1;
+                var delta = new[] {delta0, delta1};
+                theta = theta.Select((x, j) => x + delta[j]).ToList();
             }
 
-            Console.WriteLine("{0:N3}, {1:N3}", theta0, theta1);
+            Console.WriteLine("{0:N3}, {1:N3}", theta.ElementAt(0), theta.ElementAt(1));
             foreach (var sample in samples)
                 Console.WriteLine("{0}, {1:N6}", sample.Match, sample.Probability);
             Console.ReadKey();
         }
 
-        private static double Probability(Sample<Comment> s, params double[] theta)
+        private static double Probability(Sample<Comment> s, IEnumerable<double> theta)
         {
-            double thetaX = s.Features.Select((t, i) => theta[i]*t).Sum();
+            double thetaX = s.Features.Select((t, i) => theta.ElementAt(i)*t).Sum();
             return 1d/(1d + Math.Exp(-thetaX));
         }
     }
