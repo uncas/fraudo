aa0b6d480ea6fdb07eaf669458bb820b82b9c59d
Ole Lynge Soerensen <olelynge@gmail.com>
2012-05-05 00:13:20 +0200

    Collect more variables in arrays.

--- Uncas.Sandbox.Fraud/Program.cs
+++ Uncas.Sandbox.Fraud/Program.cs
@@ -23,17 +23,18 @@ namespace Uncas.Sandbox.Fraud
             {
                 foreach (var sample in samples)
                     sample.Probability = Probability(sample, theta);
-                double sum0 = 0d;
-                double sum1 = 0d;
+                List<double> sums = theta.Select(t => 0d).ToList();
                 foreach (var sample in samples)
                 {
-                    sum0 += (sample.Probability - (sample.Match ? 1 : 0))*sample.Features[0];
-                    sum1 += (sample.Probability - (sample.Match ? 1 : 0))*sample.Features[1];
+                    for (int featureIndex = 0; featureIndex < sample.Features.Count(); featureIndex++)
+                    {
+                        sums[featureIndex] +=
+                            (sample.Probability - (sample.Match ? 1 : 0))*
+                            sample.Features[featureIndex];
+                    }
                 }
 
-                double delta0 = -alpha*sum0;
-                double delta1 = -alpha*sum1;
-                var delta = new[] {delta0, delta1};
+                List<double> delta = sums.Select(s => -alpha*s).ToList();
                 theta = theta.Select((x, j) => x + delta[j]).ToList();
             }
 
