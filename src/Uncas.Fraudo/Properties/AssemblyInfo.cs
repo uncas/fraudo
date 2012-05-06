a577787e19abeaaee4ca266945a4cbe48a6512af
Ole Lynge Soerensen <olelynge@gmail.com>
2012-05-06 21:22:17 +0200

    Enable NuGet package creation.

--- src/Uncas.Fraudo/Properties/AssemblyInfo.cs
+++ src/Uncas.Fraudo/Properties/AssemblyInfo.cs
@@ -1,16 +1,12 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Uncas.Fraudo")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Fraud module")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Uncas")]
-[assembly: AssemblyProduct("Uncas.Fraudo")]
-[assembly: AssemblyCopyright("Copyright © Uncas 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,16 +17,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("bb53b702-1def-4b57-abd1-37b8f5db1944")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
