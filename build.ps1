4af32914f3dfb7e89ec087dc55dbdc8d6159eabd
Ole Lynge Soerensen <olelynge@gmail.com>
2012-05-06 20:59:40 +0200

    Use psen.

--- build.ps1
+++ build.ps1
@@ -1,2 +1,11 @@
-msbuild Uncas.Sandbox.Fraud.sln /p:Configuration=Release
-Uncas.Sandbox.Fraud\bin\Release\Uncas.Sandbox.Fraud.exe
+$task = $args[0]
+
+$psenVersion = "0.2.0.59"
+
+$psenPath = ".\packages\psen.$psenVersion\tools\psen.ps1"
+if (!(Test-Path $psenPath))
+{
+    .nuget\nuget.exe install psen -o packages -version $psenVersion
+}
+
+& $psenPath $task
